da66fd237b5d205f9b49543396e729db43186dde
jovanazivkovic99 <jovanakipar@gmail.com>
2023-07-30 12:10:15 +0200

    Created mapper for Employee

---
+++ src/main/java/com/jovanaz/ems/mapper/EmployeeMapper.java
@@ -0,0 +1,24 @@
+package com.jovanaz.ems.mapper;
+
+import com.jovanaz.ems.dto.EmployeeDto;
+import com.jovanaz.ems.entity.Employee;
+
+public class EmployeeMapper {
+    public static EmployeeDto mapToEmployeeDto(Employee employee){
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto){
+        return  new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}
