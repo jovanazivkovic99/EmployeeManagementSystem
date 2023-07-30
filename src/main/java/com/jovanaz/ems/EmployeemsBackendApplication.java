db8e47460294dd688bab155a4abe5f9e04e0c0a2
jovanazivkovic99 <jovanakipar@gmail.com>
2023-07-30 11:48:34 +0200

    Added MySql connection in application.properties

---
+++ src/main/java/com/jovanaz/ems/EmployeemsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jovanaz.ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeemsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeemsBackendApplication.class, args);
+	}
+
+}
