bc98f74ce6e9d8b2a0a847f852b87e56415a7b84
jovanazivkovic99 <jovanakipar@gmail.com>
2023-07-30 12:48:48 +0200

    Added EmployeeController + createEmployee api

---
+++ src/main/java/com/jovanaz/ems/controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package com.jovanaz.ems.controller;
+
+import com.jovanaz.ems.dto.EmployeeDto;
+import com.jovanaz.ems.service.EmployeeServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/employees")
+public class EmployeeController {
+    private final EmployeeServiceImpl employeeService;
+
+    @PostMapping
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto){
+        EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+}
